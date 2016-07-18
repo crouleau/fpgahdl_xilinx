9e4637c7a6627dfdb72cd2277e4c503cd9152e0c
Colden Rouleau <colden@rouleaufam.org>
2016-07-18 10:28:43 -0400

    Copying over the IP cores I want to use

---
+++ cf_lib/edk/pcores/axi_ad9364_digital_interface_v1_00_a/axi_ad9364_dig_if.v
@@ -0,0 +1,502 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2011(c) Analog Devices, Inc.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//     - Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     - Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in
+//       the documentation and/or other materials provided with the
+//       distribution.
+//     - Neither the name of Analog Devices, Inc. nor the names of its
+//       contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//     - The use of this software may or may not infringe the patent rights
+//       of one or more patent holders.  This license does not release you
+//       from the requirement that you obtain separate licenses from these
+//       patent holders to use this software.
+//     - Use of the software either in source or binary form, must be run
+//       on or directly connected to an Analog Devices Inc. component.
+//
+// THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED.
+//
+// IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, INTELLECTUAL PROPERTY
+// RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ***************************************************************************
+// ***************************************************************************
+// ***************************************************************************
+// ***************************************************************************
+// This interface includes both the transmit and receive components -
+// They both uses the same clock (sourced from the receiving side).
+
+`timescale 1ns/100ps
+
+module axi_ad9361_dev_if (
+
+  // physical interface (receive)
+
+  rx_clk_in_p,
+  rx_clk_in_n,
+  rx_frame_in_p,
+  rx_frame_in_n,
+  rx_data_in_p,
+  rx_data_in_n,
+
+  // physical interface (transmit)
+
+  tx_clk_out_p,
+  tx_clk_out_n,
+  tx_frame_out_p,
+  tx_frame_out_n,
+  tx_data_out_p,
+  tx_data_out_n,
+
+  // clock (common to both receive and transmit)
+
+  clk,
+
+  // receive data path interface
+
+  adc_valid,
+  adc_data_i1,
+  adc_data_q1,
+  adc_data_i2,
+  adc_data_q2,
+  adc_status,
+  adc_r1_mode,
+
+  // transmit data path interface
+
+  dac_valid,
+  dac_data_i1,
+  dac_data_q1,
+  dac_data_i2,
+  dac_data_q2,
+  dac_r1_mode,
+
+  // chipscope signals
+
+  dev_dbg_trigger,
+  dev_dbg_data);
+
+  // this parameter controls the buffer type based on the target device.
+
+  parameter   PCORE_BUFTYPE = 1; //Using Virtex 6!
+  localparam  PCORE_7SERIES = 0;
+  localparam  PCORE_VIRTEX6 = 1;
+
+  // physical interface (receive)
+
+  input           rx_clk_in_p;
+  input           rx_clk_in_n;
+  input           rx_frame_in_p;
+  input           rx_frame_in_n;
+  input   [ 5:0]  rx_data_in_p;
+  input   [ 5:0]  rx_data_in_n;
+
+  // physical interface (transmit)
+
+  output          tx_clk_out_p;
+  output          tx_clk_out_n;
+  output          tx_frame_out_p;
+  output          tx_frame_out_n;
+  output  [ 5:0]  tx_data_out_p;
+  output  [ 5:0]  tx_data_out_n;
+
+  // clock (common to both receive and transmit)
+
+  output          clk;
+
+  // receive data path interface
+
+  output          adc_valid;
+  output  [11:0]  adc_data_i1;
+  output  [11:0]  adc_data_q1;
+  output  [11:0]  adc_data_i2;
+  output  [11:0]  adc_data_q2;
+  output          adc_status;
+  input           adc_r1_mode;
+
+  // transmit data path interface
+
+  input           dac_valid;
+  input   [11:0]  dac_data_i1;
+  input   [11:0]  dac_data_q1;
+  input   [11:0]  dac_data_i2;
+  input   [11:0]  dac_data_q2;
+  input           dac_r1_mode;
+
+  // chipscope signals
+
+  output  [ 3:0]  dev_dbg_trigger;
+  output [297:0]  dev_dbg_data;
+
+  // internal registers
+
+  reg     [ 5:0]  rx_data_n = 'd0;
+  reg             rx_frame_n = 'd0;
+  reg     [11:0]  rx_data = 'd0;
+  reg     [ 1:0]  rx_frame = 'd0;
+  reg     [11:0]  rx_data_d = 'd0;
+  reg     [ 1:0]  rx_frame_d = 'd0;
+  reg             rx_error_r1 = 'd0;
+  reg             rx_valid_r1 = 'd0;
+  reg     [11:0]  rx_data_i_r1 = 'd0;
+  reg     [11:0]  rx_data_q_r1 = 'd0;
+  reg             rx_error_r2 = 'd0;
+  reg             rx_valid_r2 = 'd0;
+  reg     [11:0]  rx_data_i1_r2 = 'd0;
+  reg     [11:0]  rx_data_q1_r2 = 'd0;
+  reg     [11:0]  rx_data_i2_r2 = 'd0;
+  reg     [11:0]  rx_data_q2_r2 = 'd0;
+  reg             adc_valid = 'd0;
+  reg     [11:0]  adc_data_i1 = 'd0;
+  reg     [11:0]  adc_data_q1 = 'd0;
+  reg     [11:0]  adc_data_i2 = 'd0;
+  reg     [11:0]  adc_data_q2 = 'd0;
+  reg             adc_status = 'd0;
+  reg     [ 2:0]  tx_data_cnt = 'd0;
+  reg     [11:0]  tx_data_i1_d = 'd0;
+  reg     [11:0]  tx_data_q1_d = 'd0;
+  reg     [11:0]  tx_data_i2_d = 'd0;
+  reg     [11:0]  tx_data_q2_d = 'd0;
+  reg             tx_frame = 'd0;
+  reg     [ 5:0]  tx_data_p = 'd0;
+  reg     [ 5:0]  tx_data_n = 'd0;
+
+  // internal signals
+
+  wire    [ 3:0]  rx_frame_s;
+  wire    [ 3:0]  tx_data_sel_s;
+  wire    [ 5:0]  rx_data_ibuf_s;
+  wire    [ 5:0]  rx_data_p_s;
+  wire    [ 5:0]  rx_data_n_s;
+  wire            rx_frame_ibuf_s;
+  wire            rx_frame_p_s;
+  wire            rx_frame_n_s;
+  wire    [ 5:0]  tx_data_oddr_s;
+  wire            tx_frame_oddr_s;
+  wire            tx_clk_oddr_s;
+  wire            clk_ibuf_s;
+
+  genvar          l_inst;
+
+  // device debug signals
+
+  assign dev_dbg_trigger[0] = rx_frame[0];
+  assign dev_dbg_trigger[1] = rx_frame[1];
+  assign dev_dbg_trigger[2] = tx_frame;
+  assign dev_dbg_trigger[3] = adc_status;
+
+  assign dev_dbg_data[  5:  0] = tx_data_n;
+  assign dev_dbg_data[ 11:  6] = tx_data_p;
+  assign dev_dbg_data[ 23: 12] = tx_data_i1_d;
+  assign dev_dbg_data[ 35: 24] = tx_data_q1_d;
+  assign dev_dbg_data[ 47: 36] = tx_data_i2_d;
+  assign dev_dbg_data[ 59: 48] = tx_data_q2_d;
+  assign dev_dbg_data[ 63: 60] = tx_data_sel_s;
+  assign dev_dbg_data[ 66: 64] = tx_data_cnt;
+  assign dev_dbg_data[ 67: 67] = tx_frame;
+  assign dev_dbg_data[ 68: 68] = dac_r1_mode;
+  assign dev_dbg_data[ 69: 69] = dac_valid;
+  assign dev_dbg_data[ 81: 70] = dac_data_i1;
+  assign dev_dbg_data[ 93: 82] = dac_data_q1;
+  assign dev_dbg_data[105: 94] = dac_data_i2;
+  assign dev_dbg_data[117:106] = dac_data_q2;
+  assign dev_dbg_data[118:118] = rx_frame_p_s;
+  assign dev_dbg_data[119:119] = rx_frame_n_s;
+  assign dev_dbg_data[120:120] = rx_frame_n;
+  assign dev_dbg_data[122:121] = rx_frame;
+  assign dev_dbg_data[124:123] = rx_frame_d;
+  assign dev_dbg_data[128:125] = rx_frame_s;
+  assign dev_dbg_data[134:129] = rx_data_p_s;
+  assign dev_dbg_data[140:135] = rx_data_n_s;
+  assign dev_dbg_data[146:141] = rx_data_n;
+  assign dev_dbg_data[158:147] = rx_data;
+  assign dev_dbg_data[170:159] = rx_data_d;
+  assign dev_dbg_data[171:171] = rx_error_r1;
+  assign dev_dbg_data[172:172] = rx_valid_r1;
+  assign dev_dbg_data[184:173] = rx_data_i_r1;
+  assign dev_dbg_data[196:185] = rx_data_q_r1;
+  assign dev_dbg_data[197:197] = rx_error_r2;
+  assign dev_dbg_data[198:198] = rx_valid_r2;
+  assign dev_dbg_data[210:199] = rx_data_i1_r2;
+  assign dev_dbg_data[222:211] = rx_data_q1_r2;
+  assign dev_dbg_data[234:223] = rx_data_i2_r2;
+  assign dev_dbg_data[246:235] = rx_data_q2_r2;
+  assign dev_dbg_data[247:247] = adc_r1_mode;
+  assign dev_dbg_data[248:248] = adc_status;
+  assign dev_dbg_data[249:249] = adc_valid;
+  assign dev_dbg_data[261:250] = adc_data_i1;
+  assign dev_dbg_data[273:262] = adc_data_q1;
+  assign dev_dbg_data[285:274] = adc_data_i2;
+  assign dev_dbg_data[297:286] = adc_data_q2;
+
+  // receive data path interface
+
+  assign rx_frame_s = {rx_frame_d, rx_frame};
+
+  always @(posedge clk) begin
+    rx_data_n <= rx_data_n_s;
+    rx_frame_n <= rx_frame_n_s;
+    rx_data <= {rx_data_n, rx_data_p_s};
+    rx_frame <= {rx_frame_n, rx_frame_p_s};
+    rx_data_d <= rx_data;
+    rx_frame_d <= rx_frame;
+  end
+
+  // receive data path for single rf, frame is expected to qualify i/q msb only
+
+  always @(posedge clk) begin
+    rx_error_r1 <= ((rx_frame_s == 4'b1100) || (rx_frame_s == 4'b0011)) ? 1'b0 : 1'b1;
+    rx_valid_r1 <= (rx_frame_s == 4'b1100) ? 1'b1 : 1'b0;
+    //If the previous frame was high, and now it's low, then we've clocked in the high and low I and Q bits. We stored the first 12 bits in rx_data_d.
+    if (rx_frame_s == 4'b1100) begin
+      rx_data_i_r1 <= {rx_data_d[11:6], rx_data[11:6]}; //d, 11:16 corresponds to the first 6 bits clocked in from rx_data_in (I guess we're putting the first data in the high bits...)
+      rx_data_q_r1 <= {rx_data_d[ 5:0], rx_data[ 5:0]}; //d, 5:0 is the Q channel, the second 6 bits clocked in
+    end
+  end
+
+  // receive data path for dual rf, frame is expected to qualify i/q msb and lsb for rf-1 only
+  // The frame is longer for dual mode - it has to clock in twice as much stuff. It aligns in the 0000 and 1111 case. 
+  // We know we're aligned as long as the first two and last two bits are the same (we're clocking in at falling and rising edge of the clock)
+  always @(posedge clk) begin
+    rx_error_r2 <= ((rx_frame_s == 4'b1111) || (rx_frame_s == 4'b1100) ||
+      (rx_frame_s == 4'b0000) || (rx_frame_s == 4'b0011)) ? 1'b0 : 1'b1;
+    rx_valid_r2 <= (rx_frame_s == 4'b0000) ? 1'b1 : 1'b0;
+    if (rx_frame_s == 4'b1111) begin
+      // Channel 1 gets clocked in on a positive frame. I 11:6, Q 11:6, then I 5:0, Q 5:0
+      rx_data_i1_r2 <= {rx_data_d[11:6], rx_data[11:6]}; 
+      rx_data_q1_r2 <= {rx_data_d[ 5:0], rx_data[ 5:0]};
+    end
+    if (rx_frame_s == 4'b0000) begin
+      //Channel two comes in on a negative frame, and works the same as ch1
+      rx_data_i2_r2 <= {rx_data_d[11:6], rx_data[11:6]};
+      rx_data_q2_r2 <= {rx_data_d[ 5:0], rx_data[ 5:0]};
+    end
+  end
+
+  // receive data path mux
+
+  always @(posedge clk) begin
+    if (adc_r1_mode == 1'b1) begin
+      adc_valid <= rx_valid_r1;
+      adc_data_i1 <= rx_data_i_r1;
+      adc_data_q1 <= rx_data_q_r1;
+      adc_data_i2 <= 12'd0;
+      adc_data_q2 <= 12'd0;
+      adc_status <= ~rx_error_r1;
+    end else begin
+      adc_valid <= rx_valid_r2;
+      adc_data_i1 <= rx_data_i1_r2;
+      adc_data_q1 <= rx_data_q1_r2;
+      adc_data_i2 <= rx_data_i2_r2;
+      adc_data_q2 <= rx_data_q2_r2;
+      adc_status <= ~rx_error_r2;
+    end
+  end
+
+  // transmit data path mux (reverse of what receive does above)
+  // the count simply selets the data muxing on the ddr outputs
+
+  assign tx_data_sel_s = {tx_data_cnt[2], dac_r1_mode, tx_data_cnt[1:0]}; //concatenation
+
+  //Colden: This seems to shut down if dac valid doesn't go high again at the right point
+  //Also, if dac_valid doesn't go high after two transmission frames, I think it repeats the most
+  //recently sent data once!
+  always @(posedge clk) begin
+    if (dac_valid == 1'b1) begin
+      tx_data_cnt <= 3'b100;
+    end else if (tx_data_cnt[2] == 1'b1) begin
+      tx_data_cnt <= tx_data_cnt + 1'b1;
+    end
+    if (dac_valid == 1'b1) begin
+      tx_data_i1_d <= dac_data_i1;
+      tx_data_q1_d <= dac_data_q1;
+      tx_data_i2_d <= dac_data_i2;
+      tx_data_q2_d <= dac_data_q2;
+    end
+    case (tx_data_sel_s)
+      4'b1111: begin
+        tx_frame <= 1'b0;
+        tx_data_p <= tx_data_i1_d[ 5:0];
+        tx_data_n <= tx_data_q1_d[ 5:0];
+      end
+      4'b1110: begin
+        tx_frame <= 1'b1;
+        tx_data_p <= tx_data_i1_d[11:6];
+        tx_data_n <= tx_data_q1_d[11:6];
+      end
+      4'b1101: begin
+        tx_frame <= 1'b0;
+        tx_data_p <= tx_data_i1_d[ 5:0];
+        tx_data_n <= tx_data_q1_d[ 5:0];
+      end
+      4'b1100: begin
+        tx_frame <= 1'b1;
+        tx_data_p <= tx_data_i1_d[11:6];
+        tx_data_n <= tx_data_q1_d[11:6];
+      end
+      4'b1011: begin
+        tx_frame <= 1'b0;
+        tx_data_p <= tx_data_i2_d[ 5:0];
+        tx_data_n <= tx_data_q2_d[ 5:0];
+      end
+      4'b1010: begin
+        tx_frame <= 1'b0;
+        tx_data_p <= tx_data_i2_d[11:6];
+        tx_data_n <= tx_data_q2_d[11:6];
+      end
+      4'b1001: begin
+        tx_frame <= 1'b1;
+        tx_data_p <= tx_data_i1_d[ 5:0];
+        tx_data_n <= tx_data_q1_d[ 5:0];
+      end
+      4'b1000: begin
+        tx_frame <= 1'b1;
+        tx_data_p <= tx_data_i1_d[11:6];
+        tx_data_n <= tx_data_q1_d[11:6];
+      end
+      default: begin
+        tx_frame <= 1'b0;
+        tx_data_p <= 6'd0;
+        tx_data_n <= 6'd0;
+      end
+    endcase
+  end
+
+  generate
+  for (l_inst = 0; l_inst <= 5; l_inst = l_inst + 1) begin: g_rx_data
+
+  IBUFDS i_rx_data_ibuf (
+    .I (rx_data_in_p[l_inst]),
+    .IB (rx_data_in_n[l_inst]),
+    .O (rx_data_ibuf_s[l_inst]));    
+    
+  IDDR #(
+    .DDR_CLK_EDGE ("SAME_EDGE_PIPELINED"),
+    .INIT_Q1 (1'b0),
+    .INIT_Q2 (1'b0),
+    .SRTYPE ("ASYNC"))
+  i_rx_data_iddr (
+    .CE (1'b1),
+    .R (1'b0),
+    .S (1'b0),
+    .C (clk),
+    .D (rx_data_ibuf_s[l_inst]),
+    .Q1 (rx_data_p_s[l_inst]),
+    .Q2 (rx_data_n_s[l_inst]));
+
+  end
+  endgenerate
+
+  IBUFDS i_rx_frame_ibuf (
+    .I (rx_frame_in_p),
+    .IB (rx_frame_in_n),
+    .O (rx_frame_ibuf_s));
+
+  IDDR #(
+    .DDR_CLK_EDGE ("SAME_EDGE_PIPELINED"),
+    .INIT_Q1 (1'b0),
+    .INIT_Q2 (1'b0),
+    .SRTYPE ("ASYNC"))
+  i_rx_frame_iddr (
+    .CE (1'b1),
+    .R (1'b0),
+    .S (1'b0),
+    .C (clk),
+    .D (rx_frame_ibuf_s),
+    .Q1 (rx_frame_p_s),
+    .Q2 (rx_frame_n_s));
+
+  // transmit data interface, oddr -> obuf
+
+  generate
+  for (l_inst = 0; l_inst <= 5; l_inst = l_inst + 1) begin: g_tx_data
+
+  ODDR #(
+    .DDR_CLK_EDGE ("SAME_EDGE"),
+    .INIT (1'b0),
+    .SRTYPE ("ASYNC"))
+  i_tx_data_oddr (
+    .CE (1'b1),
+    .R (1'b0),
+    .S (1'b0),
+    .C (clk),
+    .D1 (tx_data_p[l_inst]), //Is this differential input being translated to single ended?
+    .D2 (tx_data_n[l_inst]),
+    .Q (tx_data_oddr_s[l_inst]));
+
+  OBUFDS i_tx_data_obuf (
+    .I (tx_data_oddr_s[l_inst]),
+    .O (tx_data_out_p[l_inst]),
+    .OB (tx_data_out_n[l_inst]));
+
+  end
+  endgenerate
+
+  // transmit frame interface, oddr -> obuf
+
+  ODDR #(
+    .DDR_CLK_EDGE ("SAME_EDGE"),
+    .INIT (1'b0),
+    .SRTYPE ("ASYNC"))
+  i_tx_frame_oddr (
+    .CE (1'b1),
+    .R (1'b0),
+    .S (1'b0),
+    .C (clk),
+    .D1 (tx_frame),
+    .D2 (tx_frame),
+    .Q (tx_frame_oddr_s));
+
+  OBUFDS i_tx_frame_obuf (
+    .I (tx_frame_oddr_s),
+    .O (tx_frame_out_p),
+    .OB (tx_frame_out_n));
+
+  // transmit clock interface, oddr -> obuf
+
+  ODDR #(
+    .DDR_CLK_EDGE ("SAME_EDGE"),
+    .INIT (1'b0),
+    .SRTYPE ("ASYNC"))
+  i_tx_clk_oddr (
+    .CE (1'b1),
+    .R (1'b0),
+    .S (1'b0),
+    .C (clk),
+    .D1 (1'b0),
+    .D2 (1'b1),
+    .Q (tx_clk_oddr_s));
+
+  OBUFDS i_tx_clk_obuf (
+    .I (tx_clk_oddr_s),
+    .O (tx_clk_out_p),
+    .OB (tx_clk_out_n));
+
+  // device clock interface (receive clock)
+
+  IBUFGDS i_rx_clk_ibuf (
+    .I (rx_clk_in_p),
+    .IB (rx_clk_in_n),
+    .O (clk_ibuf_s));
+
+  BUFG i_clk_gbuf (
+    .I (clk_ibuf_s),
+    .O (clk));
+
+endmodule
+
+// ***************************************************************************
+// ***************************************************************************
---
+++ cf_lib/edk/pcores/axi_ad9364_digital_interface_v1_00_a/testbench/axi_ad9364_dig_if_tb.v
@@ -0,0 +1,262 @@
+//Testbench for axi_ad9361_dev_if.v
+
+`timescale 1ns/1ns
+
+module axi_ad9361_dev_if_tb
+    (
+        //No physical interface
+        //Use "-novopt" in modelsim to ensure signal visibility
+    );
+
+    // this parameter controls the buffer type based on the target device.
+    parameter   PCORE_BUFTYPE = 1; //Using Virtex 6!
+    localparam  PCORE_7SERIES = 0;
+    localparam  PCORE_VIRTEX6 = 1;
+
+    //simulation params
+    parameter ADC_RXTX_1_MODE = 1; //Set to 1 to use just 1 tx/rx channel (you can use 1rx2tx, etc, but not supported atm)
+
+    // physical interface (Rx - input to module)
+
+    //Data clock - maxes out at 247.76MHz for LVDS, seems to be set automatically.
+    //  See "DATA_CLK" in documentation
+    // Note that we are using LVDS mode by default in the driver
+    // DATA_CLK = 2 x RX_SAMPL_FREQ (see code) for the AD9364 using LVDS (there are options if using the AD9361)
+    //RX_SAMPL_FREQ is set by a divider relating to whether there's an FIR thing going on, and divided by a bunch of clks
+    // see ad9361_calculate_rf_clock_chain
+    reg           rx_clk_in_p_tb; //simulate DATA_CLK_P in fmcomms4
+    reg           rx_clk_in_n_tb; //Need to simulate DATA_CLK_N in fmcomms4
+    reg           rx_frame_in_p_tb; //This comes from the AD9364 ("RX_FRAME_P")
+    reg           rx_frame_in_n_tb;
+    reg   [ 5:0]  rx_data_in_p_tb;
+    reg   [ 5:0]  rx_data_in_n_tb;
+
+    // physical interface (Tx - receive from module)
+
+    //tx_clk_out_p/n goes to FB_CLK_P/N
+    wire          tx_clk_out_p_tb;
+    wire          tx_clk_out_n_tb;
+    wire          tx_frame_out_p_tb;
+    wire          tx_frame_out_n_tb;
+    wire  [ 5:0]  tx_data_out_p_tb;
+    wire  [ 5:0]  tx_data_out_n_tb;
+
+    // clock (an output from the module - common to both receive and transmit)
+    wire          clk_tb;
+
+    // receive data path interface
+
+    wire          adc_valid_tb;
+    wire  [11:0]  adc_data_i1_tb;
+    wire  [11:0]  adc_data_q1_tb;
+    wire  [11:0]  adc_data_i2_tb;
+    wire  [11:0]  adc_data_q2_tb;
+    wire          adc_status_tb;
+    reg           adc_r1_mode_tb;
+
+    // transmit data path interface
+
+    reg           dac_valid_tb;
+    reg   [11:0]  dac_data_i1_tb;
+    reg   [11:0]  dac_data_q1_tb;
+    reg   [11:0]  dac_data_i2_tb;
+    reg   [11:0]  dac_data_q2_tb;
+    reg           dac_r1_mode_tb;
+
+    // chipscope signals
+    wire  [ 3:0]  dev_dbg_trigger_tb;
+    wire [297:0]  dev_dbg_data_tb;
+
+
+    //Data defines
+    //rx - what the AD9364 is supposedly receiving (alternating pattern between 1 and 2)
+    //This goes into "rx_data_in" and we should get it back from our HDL via "adc_data"
+    parameter idata1_rx = 12'o0123;
+    parameter idata2_rx = 12'o3322;
+    parameter qdata1_rx = 12'o4567;
+    parameter qdata2_rx = 12'o7664;
+    //tx - what the "HDL" we're plugged into is supposedly transmitting (alternating again, 1 and 2)
+    //This goes into the "dac_data" and we should get it back from the HDL block in question via "tx_data_out"
+    parameter idata1_tx = 12'o2064;
+    parameter idata2_tx = 12'o4402;
+    parameter qdata1_tx = 12'o1753;
+    parameter qdata2_tx = 12'o1337;
+
+    //Not sure how you're supposed to do this, but this seems to work...
+    //assign rx_clk_in_p_tb_o = rx_clk_in_p_tb;
+
+    initial
+        begin: CLK_GEN
+        rx_clk_in_p_tb = 0;
+        rx_clk_in_n_tb = 1;
+    forever
+        begin
+            #100
+            rx_clk_in_p_tb = ~rx_clk_in_p_tb;
+            rx_clk_in_n_tb = ~rx_clk_in_n_tb;
+        end
+    end
+
+    initial
+        begin: RX_FRAME_GEN
+        rx_frame_in_p_tb = 0;
+        rx_frame_in_n_tb = 1;
+    forever
+        begin
+            //in 1rx 1tx mode, switch the frame at every positive clock edge
+            //See page 110 of the AD9361 Reference Manual (rx_clk_in_p is hooked up to DATA_CLK_P)
+            if(ADC_RXTX_1_MODE == 1) begin
+                @(posedge rx_clk_in_p_tb)
+                #1;
+                rx_frame_in_p_tb = ~rx_frame_in_p_tb;
+                rx_frame_in_n_tb = ~rx_frame_in_n_tb;
+            //in 2rx 2tx mode, switch the frame every other positive clock edge
+            end else begin
+                @(posedge rx_clk_in_p_tb)
+                #1
+                @(posedge rx_clk_in_p_tb)
+                #1
+                rx_frame_in_p_tb = ~rx_frame_in_p_tb;
+                rx_frame_in_n_tb = ~rx_frame_in_n_tb;
+            end
+        end
+    end
+
+    //Insert data generation here... for now making it static (still have to clock it because of high/low bit transitions)
+    //This block simulates data coming from the AD9364 that was received from RF
+    initial
+        begin: RX_DATA_GEN
+            rx_data_in_p_tb = 6'o00;
+            //wire up dac_r1_mode and adc_r1_mode based on the param value (dumb place to put this but w/e)
+            if(ADC_RXTX_1_MODE == 1) begin
+                dac_r1_mode_tb = 1'b1;
+                adc_r1_mode_tb = 1'b1;
+            end else begin
+                dac_r1_mode_tb = 1'b0;
+                adc_r1_mode_tb = 1'b0;
+            end
+    forever
+        begin
+            //in 1rx 1tx mode, the high bits go at the positive edge of the frame (I then Q)
+            //and low bits do the same at the negative edge of the frame
+            if(ADC_RXTX_1_MODE == 1) begin
+                //Transmit the first I/Q data
+                @(posedge rx_frame_in_p_tb)
+                #1
+                rx_data_in_p_tb = idata1_rx[11:6];
+                //Now wait for the negative edge of the clock to put in the Q data
+                @(negedge rx_clk_in_p_tb)
+                #1
+                rx_data_in_p_tb = qdata1_rx[11:6];
+
+                @(negedge rx_frame_in_p_tb) //Not sure if making this sequential is "proper"
+                #1
+                rx_data_in_p_tb = idata1_rx[5:0];
+
+                @(negedge rx_clk_in_p_tb)
+                #1
+                rx_data_in_p_tb = qdata1_rx[5:0];
+
+                //now transmit some different data
+                @(posedge rx_frame_in_p_tb)
+                #1
+                rx_data_in_p_tb = idata2_rx[11:6];
+
+                @(negedge rx_clk_in_p_tb)
+                #1
+                rx_data_in_p_tb = qdata2_rx[11:6];
+
+                @(negedge rx_frame_in_p_tb)
+                #1
+                rx_data_in_p_tb = idata2_rx[5:0];
+
+                @(negedge rx_clk_in_p_tb)
+                #1
+                rx_data_in_p_tb = qdata2_rx[5:0];
+
+            //In 2rx 2tx mode, you do all the 11:6, I then Q, then 5:0, I then Q, for channel 1 on the pos edge of the frame
+            //and the same for channel 2 but on the negative edge of the frame
+            end else begin
+                rx_data_in_p_tb = 6'o7777; //TODO: Implementme! (making everything the same right now)
+            end
+        end
+    end
+
+    always @(*) begin
+        rx_data_in_n_tb <= ~rx_data_in_p_tb;
+    end
+    //Implement DAC interface
+    initial
+        begin: TX_DATA_GEN
+            dac_valid_tb = 1'b0;
+            dac_data_i1_tb = 1'b0;
+            dac_data_q1_tb = 1'b0;
+            dac_data_i2_tb = 1'b0;
+            dac_data_q2_tb = 1'b0;
+    forever
+        begin
+            //Transmitter IP is synchronized to clk, which is synchronized to the differential rx_clk_in_p
+            if(ADC_RXTX_1_MODE == 1) begin
+                @(posedge clk_tb) //This also serves to clock out the 2nd two frames (I/Q, 5:0)
+                #1
+                dac_data_i1_tb = idata1_tx;
+                dac_data_q1_tb = qdata1_tx;
+                dac_valid_tb = 1'b1; //This preps the dev_if logic to take in the data
+
+                @(posedge clk_tb) //When this occurs, the gets taken in, and later clocked out on I/Q, 11:6
+                #1
+                dac_valid_tb = 1'b0; //With the data taken in, valid must go low again
+
+                @(posedge clk_tb) //This is just here so we can send alternating data packets
+                #1
+                dac_data_i1_tb = idata2_tx;
+                dac_data_q1_tb = qdata2_tx;
+                dac_valid_tb = 1'b1;
+
+                @(posedge clk_tb)
+                #1
+                dac_valid_tb = 1'b0;
+
+            end else begin
+                //Not implemented atm
+                dac_data_i1_tb = 12'o0000;
+                dac_data_q1_tb = 12'o0000;
+                dac_valid_tb = 1'b0;
+            end
+        end
+    end
+
+    axi_ad9361_dev_if #(
+        .PCORE_BUFTYPE (PCORE_BUFTYPE))
+    i_dev_if (
+        .rx_clk_in_p (rx_clk_in_p_tb),
+        .rx_clk_in_n (rx_clk_in_n_tb),
+        .rx_frame_in_p (rx_frame_in_p_tb),
+        .rx_frame_in_n (rx_frame_in_n_tb),
+        .rx_data_in_p (rx_data_in_p_tb),
+        .rx_data_in_n (rx_data_in_n_tb),
+        .tx_clk_out_p (tx_clk_out_p_tb),
+        .tx_clk_out_n (tx_clk_out_n_tb),
+        .tx_frame_out_p (tx_frame_out_p_tb),
+        .tx_frame_out_n (tx_frame_out_n_tb),
+        .tx_data_out_p (tx_data_out_p_tb),
+        .tx_data_out_n (tx_data_out_n_tb),
+        .clk (clk_tb),
+        .adc_valid (adc_valid_s_tb),
+        .adc_data_i1 (adc_data_i1_tb),
+        .adc_data_q1 (adc_data_q1_tb),
+        .adc_data_i2 (adc_data_i2_tb),
+        .adc_data_q2 (adc_data_q2_tb),
+        .adc_status (adc_status_tb),
+        .adc_r1_mode (adc_r1_mode_tb),
+        .dac_valid (dac_valid_tb),
+        .dac_data_i1 (dac_data_i1_tb),
+        .dac_data_q1 (dac_data_q1_tb),
+        .dac_data_i2 (dac_data_i2_tb),
+        .dac_data_q2 (dac_data_q2_tb),
+        .dac_r1_mode (dac_r1_mode_tb),
+        .dev_dbg_trigger (dev_dbg_trigger_tb),
+        .dev_dbg_data (dev_dbg_data_tb)
+    );
+
+endmodule
